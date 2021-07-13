a72e8f66b3d51a7647a3cef459c8fff04673beb5
subhash-CN <85877879+subhash-CN@users.noreply.github.com>
2021-07-13 17:57:07 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@ fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
 
-    if { true } { /* ... */ }
-    
-    
-     if x != y {
-         println!("Success!");
-     }
-     if x != y {
-         println!("Success!");
-     }
+    if x != y {
+        println!("Success!");
+    }
     println!("Hello, world!");
 }
